2057d34e1e178a7dfc3d6a5eabb5dc7b5ce2caf6
SoMysrie <rethores@gmail.com>
2016-06-18 16:59:44 +0200

    Ajout des fichiers csv

--- src/example/matplot.py
+++ src/example/matplot.py
@@ -2,6 +2,7 @@
 # -*- coding : utf-8 -*-
 
 import matplotlib.pyplot as plt
+
 plt.plot([1,2,3,4])
 plt.ylabel('some numbers')
 plt.show()
--- src/main.py
+++ src/main.py
@@ -1,3 +1,18 @@
 #!/usr/bin/python
 # -*- coding : utf-8 -*-
 
+import os
+import csv
+from Tkinter import *
+import matplotlib.pyplot as plt
+
+fname1 = "donnees/API_6_DS2_fr_csv_v2.csv"
+file = open(fname1, "r")
+
+try:
+    reader = csv.reader(file)
+    for row in reader:
+		if row:
+			print row[0],row[2]
+finally:
+	file.close() 
